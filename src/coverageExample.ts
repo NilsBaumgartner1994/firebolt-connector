15aadd5152e5c6cb772985220f6cd5fd858327d4
Nils <nilsbaumgartner1994@gmail.com>
2021-07-06 23:06:44 +0200

    Added code change to test sonarcloud

--- src/coverageExample.ts
+++ src/coverageExample.ts
@@ -23,7 +23,7 @@ export default class CoverageExample {
     } else {
       if (number < 10) {
         if (number < 4) {
-          return "adding for sonarcloud";
+          return "adding for sonarcloud change";
         } else {
           return 'go';
         }
